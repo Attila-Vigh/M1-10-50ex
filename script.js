bc61e711dd25a9176ff46c8c416c5851fde91317
attila <attilavigh.contact@gmail.com>
2021-05-28 15:05:34 +0100

    Ex.21: Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"

---
+++ script.js
@@ -0,0 +1,17 @@
+// Ex.21: Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+let x = "John" 
+let y = "Doe"
+console.log(`${x} <> ${y}`);
+
+
+// JS EXERCISES
+
+// Ex.22: Create an object with properties such name, surname, email
+// Ex.23: Delete Email from the previously created object
+// Ex.24: Create an array with 10 strings in it
+// Ex.25: Print in the console every string in the previous array
+// Ex.26: Create an array with 100 random numbers in it
+// Ex.27: Wrote a function to get the MAX and the MIN from the previously created array
+// Ex.28: Create an array of arrays, in which every array has 10 random numbers
+// Ex.29: Create a function that gets 2 arrays and returns the longest one
+// Ex.30: Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
