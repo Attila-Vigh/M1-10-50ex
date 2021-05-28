666f2235f6c923a72c8f506774a686682c665b63
attila <attilavigh.contact@gmail.com>
2021-05-28 15:08:34 +0100

    Ex.22: Create an object with properties such name, surname, email

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 let x = "John" 
 let y = "Doe"
 console.log(`${x} <> ${y}`);
+// Ex.22: Create an object with properties such name, surname, email
+
+let person = {
+    name : "", 
+    surname : "", 
+    email : ""
+}
 
 
 // JS EXERCISES
 
-// Ex.22: Create an object with properties such name, surname, email
 // Ex.23: Delete Email from the previously created object
 // Ex.24: Create an array with 10 strings in it
 // Ex.25: Print in the console every string in the previous array
